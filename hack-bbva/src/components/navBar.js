b795117a896b5e6b43edaf41df0961f2bec76832
DanielRamGe <314027979@pcpuma.acatlan.unam.mx>
2021-10-23 18:16:18 -0500

    add home page

---
+++ hack-bbva/src/components/navBar.js
@@ -0,0 +1,67 @@
+import './navBar.css';
+import React, { useState } from "react";
+import { Navbar, Container, Col, Row } from "react-bootstrap";
+import { AiOutlineMenu, AiOutlineSearch } from "react-icons/ai";
+import { Icon } from "semantic-ui-react";
+// import { CustomDrawer } from "./CustomComponent";
+
+export const NavBar = ({ nombreUsuario }, props) => {
+  const [open, setOpen] = useState(false)
+  const showDrawer = () => setOpen(!open)
+  return (
+    <div>
+      <Navbar className="mainNav">
+        <Container
+          style={{ marginTop: "0", marginBottom: "0", color: "white" }}
+          fluid
+        >
+
+          <Navbar.Brand
+            className="justify-content-center"
+            style={{ color: "white", marginLeft: "2%" }}
+          >
+            <AiOutlineMenu style={{ cursor: "pointer"}} size={22} onClick={showDrawer} />
+            {" "}
+            Menú
+          </Navbar.Brand>
+          <Navbar.Brand
+            className="justify-content-center"
+            style={{ color: "white", marginLeft: "33%", }}
+          >
+            <img
+                className="d-block w-100"
+                src="https://www.bbva.mx/content/dam/public-web/global/images/logos/logo_bbva_blanco.svg"
+                alt="Second slide"
+                style={{  height: "2.5rem" }}
+            />
+          </Navbar.Brand>
+          <Navbar.Collapse className="justify-content-end">
+            <Navbar.Brand style={{ color: "white" }}><AiOutlineSearch /></Navbar.Brand>
+            <Navbar.Brand href="#home" style={{ color: "white" }}>Registrate</Navbar.Brand>
+            <Navbar.Brand href="#home" style={{ color: "white" }}>Acceso</Navbar.Brand>
+          </Navbar.Collapse>
+        </Container>
+      </Navbar>
+      
+        <div className={open ? "container-drawer active" : "container-drawer"}>
+          <Row>
+            <Col xs={10} md={9} lg={4} /*className="content"*/>
+              {props.children}
+            </Col>
+          </Row>
+        </div>
+        <style type="text/css">
+          {`
+            .content {
+              width: 180vh;
+            }
+            @media (max-width: 769px) {
+              .content {
+                width: 100vh;
+              }
+            }
+          `}
+        </style>
+    </div>
+  );
+};
--- hack-bbva/src/pages/home/index.js
+++ hack-bbva/src/pages/home/index.js
@@ -1,20 +1,42 @@
 
-
-
+import React from "react";
+import { NavBar} from "../../components/navBar";
+import { Carousel, Image, Card, Button } from "react-bootstrap";
+import { useState} from "react";
 export const Home = () => {
+
+   
     
+    const [index, setIndex] = useState(0);
+    
+    const handleSelect = (selectedIndex, e) => {
+        setIndex(selectedIndex);
+    }
   return (
-      <div><p>Home page</p></div>
+    <div>
+        <NavBar nombreAdscripcionUsuario="Usario" />
+        <Carousel variant="dark" >
+            <Carousel.Item>
+                <Image src="https://static.comunicae.com/photos/notas/1220121/1606469651_captio_and_bbva.png" fluid />
+                <Carousel.Caption>
+                <Card style={{ marginBottom: '20rem', width: '25rem' }} variant="top" >
+                    <Card.Body style={{textAlign:'left'}}>
+                        <Card.Title>¿Eres Pyme?</Card.Title>
+                        <Card.Text>
+                        Tenemos una cuenta de ahorros ideal para ti,  regístrate y abre tu cuenta en solo 3 minutos.
+                        </Card.Text>
+                        <div className="float-left">
+                        <Button  variant="primary" style={{ width: '15rem' }}>Registrate</Button>
+
+                        </div>
+                    </Card.Body>
+                </Card>
+                </Carousel.Caption>
+            </Carousel.Item>
+        </Carousel>
+    </div>
   );
 }
-
-
-
-
-
-
-
-
 
 
 
